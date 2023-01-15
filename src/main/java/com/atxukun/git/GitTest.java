046c48625158aac190d6ec06307c7f41fa1c60ba
zhaowenmeng <2492322039@qq.com>
2023-01-15 19:16:03 +0800

    first commit22

--- src/main/java/com/atxukun/git/GitTest.java
+++ src/main/java/com/atxukun/git/GitTest.java
@@ -3,5 +3,6 @@ package com.atxukun.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println( "hello git IDEA");
+        System.out.println( "hello git IDEA2");
     }
 }
