964b36eb2ebc055ee8d572ebea055ae1e40c016b
zhaowenmeng <2492322039@qq.com>
2023-01-15 19:16:41 +0800

    first commit33

--- src/main/java/com/atxukun/git/GitTest.java
+++ src/main/java/com/atxukun/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println( "hello git IDEA");
         System.out.println( "hello git IDEA2");
+        System.out.println( "hello git IDEA3");
     }
 }
